0111ae3b001af20f0c743e21c5b0a832f4e383fa
Adarsh <adarshregmi1@gmail.com>
2023-05-23 22:22:55 +0530

    updated

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,12 +1,12 @@
 import { Client, Databases, ID } from 'appwrite';
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('6467cf114ccfab3085b6');               // Your project ID
+    .setProject('');               // Your project ID
 
 const databases = new Databases(client);
 const promise = databases.createDocument(
-    '646cdcc9b0508d521af3',
-    '646cdd086b2f96729ea7',
+    '',
+    '',
     ID.unique(),
     {}
 );
