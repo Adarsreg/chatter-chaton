256a40b39eed38ada9c72b23911468c2f68a9b8f
Adarsh <adarshregmi1@gmail.com>
2023-05-30 01:52:15 +0530

    add auth.ts|appwriteAdapter|init next-auth

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,38 @@
+import { NextAuthOptions } from "next-auth";
+import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
+
+import AppwriteAdapter from './AppwriteAdapter'
+import { Client } from "appwrite";
+const appwriteClient = new Client();
+
+function getGoogleCredentials() {
+    const clientId = process.env.GOOGLE_CLIENT_ID
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET
+
+    if (!clientId || clientId.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_ID')
+    }
+
+    if (!clientSecret || clientSecret.length === 0) {
+        throw new Error('Missing GOOGLE_CLIENT_SECRET')
+    }
+
+    return { clientId, clientSecret }
+}
+export const authOptions: NextAuthOptions = {
+    adapter: AppwriteAdapter(appwriteClient),
+    session: {
+        strategy: "jwt"
+    },
+    pages: {
+        signIn: '/login',
+    },
+    providers: [
+        GoogleProvider({
+            clientId: getGoogleCredentials().clientId,
+            clientSecret: getGoogleCredentials().clientSecret,
+        }),
+    ],
+
+}
---
+++ src/app/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,6 @@
+//any requests made to this file will be handled by next-auth
+import { NextAuthOptions } from "next-auth";
+
+/* export const authOptions: NextAuthOptions = {
+    adapter: 
+} */
