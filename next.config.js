06c0631e307b3454a2f089c8c57e0e6f63c753fa
Adarsh <adarshregmi1@gmail.com>
2023-05-30 18:51:44 +0530

    add next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+
+        API_ENDPOINT: process.env.API_ENDPOINT,
+        PROJECT_ID: process.env.PROJECT_ID,
+        DATABASE_ID: process.env.DATABASE_ID,
+        COLLECTION_ID: process.env.COLLECTION_ID,
+
+    }
+}
 
 module.exports = nextConfig
