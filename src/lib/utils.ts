63e671761e8ef58e86bb33e842691b88155085c0
Adarsh <adarshregmi1@gmail.com>
2023-07-15 15:30:52 +0530

    Updated utils and interface in db.d.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,3 +8,7 @@ export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
 
+export function chatHrefConstructor(id1: string, id2: string) {
+    const sortedIds = [id1, id2].sort()
+    return `${sortedIds[0]}--${sortedIds[1]}`
+}
--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -4,3 +4,21 @@ interface User {
     image: string
     id: string
 }
+
+interface Chat {
+    id: string
+    messages: Message[]
+}
+interface Message {
+    id: string
+    senderId: string
+    receiverId: string
+    text: string
+    timestamp: number
+}
+
+interface FriendRequest {
+    id: string
+    senderId: string
+    receiverId: string
+}
