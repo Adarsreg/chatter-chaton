985c9c18fab2a08356056dec73151cf51a441708
Adarsh <adarshregmi1@gmail.com>
2023-07-16 02:19:53 +0530

    fetched and displayed the chat friend when clicked

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from '@component/lib/auth'
 import { db } from '@component/lib/db'
 import { messageArrayValidator } from '@component/lib/validations/message'
 import { getServerSession } from 'next-auth'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
 import { FC } from 'react'
 
@@ -43,7 +44,34 @@ const page = async ({ params }: pageProps) => {
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User
   const initialMessages = await getChatMessages(chatId)
 
-  return <div>{params.chatId}</div>
-}
+  return (
+    <div className='flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)]'>
+      <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200'>
+        <div className='relative flex items-center space-x-4'>
+          <div className='relative'>
+            <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
+              <Image
+                fill
+                referrerPolicy='no-referrer'
+                src={chatPartner.image}
+                alt={`${chatPartner.name} profile picture`}
+                className='rounded-full'
+              />
+            </div>
+          </div>
 
+          <div className='flex flex-col leading-tight'>
+            <div className='text-xl flex items-center'>
+              <span className='text-gray-700 mr-3 font-semibold'>
+                {chatPartner.name}
+              </span>
+            </div>
+
+            <span className='text-sm text-gray-600'>{chatPartner.email}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 export default page
