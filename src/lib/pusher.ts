49dc3479ac3d6cd2adc489c6bfce933ee1f5692c
Adarsh <adarshregmi1@gmail.com>
2023-08-03 23:07:35 +0530

    fixed naming error at pusher.ts

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -4,7 +4,7 @@ import PusherClient from 'pusher-js';
 export const pusherServer = new PusherServer(
     {
         appId: process.env.PUSHER_APP_ID!,
-        key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
+        key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
         secret: process.env.PUSHER_APP_SECRET!,
         cluster: 'ap2',
         useTLS: true
