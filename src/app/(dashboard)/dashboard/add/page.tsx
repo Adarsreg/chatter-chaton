57d8a69081a3d13a8b103a196d49f040e2c17457
Adarsh <adarshregmi1@gmail.com>
2023-08-15 17:24:18 +0530

    .

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -3,6 +3,7 @@ import AddFriendButton from '@component/components/AddFriendButton'
 import { FC } from 'react'
 
 const page: FC = () => {
+
   return (
     <main className='pt-8'>
       <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
