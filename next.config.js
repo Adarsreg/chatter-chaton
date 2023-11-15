ae5f1040b1edc2b4033109e80b7a1bf95669efeb
Adarsh <adarshregmi1@gmail.com>
2023-11-15 17:28:13 +0530

    updated url for production

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-
-        API_ENDPOINT: process.env.API_ENDPOINT,
-        PROJECT_ID: process.env.PROJECT_ID,
-        DATABASE_ID: process.env.DATABASE_ID,
-        COLLECTION_ID: process.env.COLLECTION_ID,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
         GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         UPSTASH_REDIS_URL: process.env.UPSTASH_REDIS_URL,
