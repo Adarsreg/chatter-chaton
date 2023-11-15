922dfb89d4747e1f61b04ac1d4116e6fd48572ad
Adarsh <adarshregmi1@gmail.com>
2023-11-15 19:31:24 +0530

    Fixed realtime communication not working| addawait

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,74 +1,82 @@
 //mandatory route.ts file for routing
 //handling post req
-import { fetchRedis } from "@component/helpers/redis"
-import { authOptions } from "@component/lib/auth"
-import { db } from "@component/lib/db"
-import { pusherServer } from "@component/lib/pusher"
-import { toPusherKey } from "@component/lib/utils"
-import { addFriendValidator } from "@component/lib/validations/add-friend"
-import { getServerSession } from "next-auth"
-import { z } from "zod"
+import { fetchRedis } from "@component/helpers/redis";
+import { authOptions } from "@component/lib/auth";
+import { db } from "@component/lib/db";
+import { pusherServer } from "@component/lib/pusher";
+import { toPusherKey } from "@component/lib/utils";
+import { addFriendValidator } from "@component/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
 
 export async function POST(req: Request) {
-    try {
+  try {
+    const body = await req.json();
+    const { email: emailToAdd } = addFriendValidator.parse(body.email);
 
-        const body = await req.json()
-        const { email: emailToAdd } = addFriendValidator.parse(body.email)
+    const RESTResponse = await fetch(
+      `${process.env.UPSTASH_REDIS_URL}/get/user:email:${emailToAdd}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.UPSTASH_REDIS_TOKEN}`,
+        },
+        cache: "no-store",
+      }
+    );
+    //user:email:simchettri123@gmail.com => source url
 
-        const RESTResponse = await fetch(`${process.env.UPSTASH_REDIS_URL}/get/user:email:${emailToAdd}`, {
-            headers: {
-                Authorization: `Bearer ${process.env.UPSTASH_REDIS_TOKEN}`,
-            },
-            cache: "no-store",
-        }
-        )
-        //user:email:simchettri123@gmail.com => source url
-
-        const data = await RESTResponse.json() as { result: string | null }
-        const idToAdd = data.result
-        if (!idToAdd) {
-            return new Response('This person does not exist', { status: 400 })
-        }
-        const session = await getServerSession(authOptions)
-        if (!session) {
-            return new Response("Unauthorized", { status: 401 })
-        }
-        if (idToAdd === session.user.id) {
-            return new Response("You cannot add yourself as a friend", { status: 400 })
-
-        }
-
-        //check for already added user using a custom helper function
-        const isAlreadyAdded = await fetchRedis('sismember', `user:${idToAdd}:incoming_friend_requests`, session.user.id) as 0 | 1
-
-        const isAlreadyFriends = await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd) as 0 | 1
-        if (isAlreadyAdded) {
-            return new Response('Already added this user', { status: 400 })
-        }
-
-        if (isAlreadyFriends) {
-            return new Response('Already friends with this user', { status: 400 })
-        }
-
-        pusherServer.trigger(
-            toPusherKey(`user:${idToAdd}:incoming_friend_requests`), 'incoming_friend_requests',
-            {
-                senderId: session.user.id,
-                senderEmail: session.user.email
-
-            }
-        )
-
-        //valid request, send friend request
-        db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id)
-
-        return new Response('OK')
-
+    const data = (await RESTResponse.json()) as { result: string | null };
+    const idToAdd = data.result;
+    if (!idToAdd) {
+      return new Response("This person does not exist", { status: 400 });
     }
-    catch (error) {
-        if (error instanceof z.ZodError) {
-            return new Response("Invalid request payload", { status: 422 })
-        }
-        return new Response('Invalid request', { status: 400 })
+    const session = await getServerSession(authOptions);
+    if (!session) {
+      return new Response("Unauthorized", { status: 401 });
     }
+    if (idToAdd === session.user.id) {
+      return new Response("You cannot add yourself as a friend", {
+        status: 400,
+      });
+    }
+
+    //check for already added user using a custom helper function
+    const isAlreadyAdded = (await fetchRedis(
+      "sismember",
+      `user:${idToAdd}:incoming_friend_requests`,
+      session.user.id
+    )) as 0 | 1;
+
+    const isAlreadyFriends = (await fetchRedis(
+      "sismember",
+      `user:${session.user.id}:friends`,
+      idToAdd
+    )) as 0 | 1;
+    if (isAlreadyAdded) {
+      return new Response("Already added this user", { status: 400 });
+    }
+
+    if (isAlreadyFriends) {
+      return new Response("Already friends with this user", { status: 400 });
+    }
+
+    await pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
+    //valid request, send friend request
+    db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
+
+    return new Response("OK");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request payload", { status: 422 });
+    }
+    return new Response("Invalid request", { status: 400 });
+  }
 }
--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -1,76 +1,81 @@
-
-import { fetchRedis } from "@component/helpers/redis"
-import { authOptions } from "@component/lib/auth"
-import { db } from "@component/lib/db"
-import { pusherServer } from "@component/lib/pusher"
-import { toPusherKey } from "@component/lib/utils"
-import { messageValidator } from "@component/lib/validations/message"
-import { nanoid } from "nanoid"
-import { getServerSession } from "next-auth"
-
+import { fetchRedis } from "@component/helpers/redis";
+import { authOptions } from "@component/lib/auth";
+import { db } from "@component/lib/db";
+import { pusherServer } from "@component/lib/pusher";
+import { toPusherKey } from "@component/lib/utils";
+import { messageValidator } from "@component/lib/validations/message";
+import { nanoid } from "nanoid";
+import { getServerSession } from "next-auth";
 
 export async function POST(req: Request) {
-    try {
+  try {
+    const { text, chatId }: { text: string; chatId: string } = await req.json();
 
-        const { text, chatId }: { text: string, chatId: string } = await req.json()
+    const session = await getServerSession(authOptions);
 
-        const session = await getServerSession(authOptions)
+    if (!session) return new Response("Unauthorized", { status: 401 });
 
-        if (!session) return new Response('Unauthorized', { status: 401 })
+    const [userId1, userId2] = chatId.split("--");
 
+    if (session.user.id !== userId1 && session.user.id !== userId2) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+    const friendId = session.user.id === userId1 ? userId2 : userId1;
+    const friendList = (await fetchRedis(
+      "smembers",
+      `user:${session.user.id}:friends`
+    )) as string[];
+    const isFriend = friendList.includes(friendId);
 
-        const [userId1, userId2] = chatId.split('--')
-
-
-        if (session.user.id !== userId1 && session.user.id !== userId2) {
-            return new Response('Unauthorized', { status: 401 })
-        }
-        const friendId = session.user.id === userId1 ? userId2 : userId1
-        const friendList = await fetchRedis('smembers', `user:${session.user.id}:friends`) as string[]
-        const isFriend = friendList.includes(friendId)
-
-        if (!isFriend) {
-            return new Response('Unauthorized', { status: 401 })
-        }
-
-
-        const rawsender = (await fetchRedis('get', `user:${session.user.id}`)) as string
-
-        const sender = JSON.parse(rawsender) as User
-        const timestamp = Date.now()
-        // send message to redis
-
-        const messageData: Message = {
-            id: nanoid(),
-            senderId: session.user.id,
-            text,
-            timestamp,
-        }
-        const message = messageValidator.parse(messageData)
-        //sends message to all the connected clients(chat rooms)
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
-
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
-            //toast notifcation for new message
-            ...message,
-            senderImg: sender.image,
-            senderName: sender.name
-        })
-
-        await db.zadd(`chat:${chatId}:messages`, {
-            score: timestamp,
-            member: JSON.stringify(message)
-        })
-
-        return new Response('Ok')
-    } catch (error) {
-        if (error instanceof Error) {
-
-            return new Response(error.message, { status: 500 })
-
-        }
-
-        return new Response('Internal Server Error ', { status: 500 })
+    if (!isFriend) {
+      return new Response("Unauthorized", { status: 401 });
     }
 
+    const rawsender = (await fetchRedis(
+      "get",
+      `user:${session.user.id}`
+    )) as string;
+
+    const sender = JSON.parse(rawsender) as User;
+    const timestamp = Date.now();
+    // send message to redis
+
+    const messageData: Message = {
+      id: nanoid(),
+      senderId: session.user.id,
+      text,
+      timestamp,
+    };
+    const message = messageValidator.parse(messageData);
+    //sends message to all the connected clients(chat rooms)
+    await pusherServer.trigger(
+      toPusherKey(`chat:${chatId}`),
+      "incoming-message",
+      message
+    );
+
+    await pusherServer.trigger(
+      toPusherKey(`user:${friendId}:chats`),
+      "new_message",
+      {
+        //toast notifcation for new message
+        ...message,
+        senderImg: sender.image,
+        senderName: sender.name,
+      }
+    );
+
+    await db.zadd(`chat:${chatId}:messages`, {
+      score: timestamp,
+      member: JSON.stringify(message),
+    });
+
+    return new Response("Ok");
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 });
+    }
+
+    return new Response("Internal Server Error ", { status: 500 });
+  }
 }
